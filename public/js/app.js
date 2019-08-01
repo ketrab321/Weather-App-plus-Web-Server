e8dc44114a4a30cf819bde29b1ac6988ec87e432
Bartosz Stajnowski <bstajnowski@it-dev.pl>
2019-08-01 15:51:14 +0200

    Fetching weather information from query html supported by server itself

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,19 @@
 console.log("Test");
+
+fetch('http://puzzle.mead.io/puzzle').then((response)=>{
+    response.json().then((data)=>{
+        console.log(data);
+    })
+})
+
+
+fetch('http://localhost:3000/weather?address=Koszalin').then((response)=>{
+    response.json().then((data)=>{
+        if(data.error){
+            console.log(data.error);
+        }else{
+            console.log(data.location);
+            console.log(data.forecast);
+        }
+    })
+})
--- src/app.js
+++ src/app.js
@@ -45,7 +45,7 @@ app.get('/weather',(req,res)=>{
             error: 'You must provide address field'
         });
     }
-    geocode(req.query.address,(error,{latitude,longitude,location})=>{
+    geocode(req.query.address,(error,{latitude,longitude,location}={})=>{
         if(error){
             return res.send({error});
         }
