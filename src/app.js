9b523ce09d1d4be7412101507b846fd0479822a3
Bartosz Stajnowski <bstajnowski@it-dev.pl>
2019-08-01 15:05:20 +0200

    Created front end of weather application

---
+++ src/app.js
@@ -0,0 +1,85 @@
+const path = require('path');
+const express = require('express');
+const hbs = require('hbs');
+
+//Define paths for express config
+const publicDirectoryPath = path.join(__dirname,'..','public');
+const viewsPath = path.join(__dirname,'..','templates','views');
+const partialsPath = path.join(__dirname,'..','templates','partials');
+
+
+const app = express();
+//setup static directory to serve
+app.use(express.static(publicDirectoryPath));
+//setup handlebars engine
+app.set('view engine','hbs');
+app.set('views',viewsPath);
+hbs.registerPartials(partialsPath);
+
+app.get('',(req,res)=>{
+    res.render('index',{
+        title: "Title from app",
+        name: "My name"
+    });
+})
+app.get('/help',(req,res)=>{
+    res.render('help',{
+        title: "Help me",
+        name: "Grzechu"
+    })
+})
+
+app.get('/about',(req,res)=>{
+    res.render('about',{
+        title: "About me",
+        name: "Krzychu"
+    });
+})
+
+app.get('/weather',(req,res)=>{
+    if(!req.query.address)
+    {
+        return res.send({
+            error: 'You must provide address field'
+        });
+    }
+    res.send({
+        forecast: 'Słoneczko i ładnie ogólnie',
+        location: 'Legnica',
+        address: req.query.address
+    });
+    
+})
+
+app.get('/products',(req,res)=>{
+    if(!req.query.key){
+        return res.send({
+            error: 'You must provide key term'
+        })
+    }
+    
+        console.log(req.query.key);
+        res.send({
+            products: []
+        })
+    
+})
+app.get('/help/*',(req,res)=>{
+    res.render('404page',{
+        title: '404',
+        errorMessage: 'Help article not found',
+        name: 'Johny Bravo'
+    });
+})
+app.get('*',(req,res)=>{
+    res.render('404page',{
+        title: '404',
+        errorMessage: 'not found',
+
+        name: 'Darth Vader'
+    });
+})
+
+app.listen(3000,()=>{
+    console.log("Server is up on port 3000");
+});
